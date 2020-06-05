0f18f0f7d969d80667de4fc56c5fa5fc6027c7bd
Ching-Jen518 <st86714@hotmail.com>
2020-06-05 13:08:24 +0800

    finish Homepage

--- PathPlus/Controllers/HomeController.cs
+++ PathPlus/Controllers/HomeController.cs
@@ -144,7 +144,9 @@ namespace PathPlus.Controllers
                     {
 
                         fileName = DateTime.Now.ToString().Replace("/", "").Replace(":", "").Replace(" ", "").Replace("上午", "").Replace("下午", "") + (i + 1).ToString() + ".jpg";
-                        newphoto.Photo = Server.MapPath("~/Photo/" + fileName);
+                        photo[i].SaveAs(Server.MapPath("~/Photo/" + fileName));
+                        newphoto.Photo = fileName;
+                        
 
                         db.PostPhoto.Add(newphoto);
                         db.SaveChanges();
